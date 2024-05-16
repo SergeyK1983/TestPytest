40235bd554004e40cda4226953a38255d5abc7c8
SergeyK1983 <ssp-serg@yandex.ru>
2024-05-16 21:00:23 +0300

    correction test_models

--- testpoject/testapp/tests/test_models.py
+++ testpoject/testapp/tests/test_models.py
@@ -2,18 +2,12 @@ import pytest
 
 
 @pytest.mark.django_db
-def test_current_model_factory(current_model_factory, rel_current_factory):
+def test_current_model_factory(current_model_factory):
     model_cu = current_model_factory.create()
-    # rm = rel_current_factory.create()
-
-    print("model_cu: ", model_cu.id)
-    # print("rm: ", rm.current)
-
     assert model_cu.id
 
 
 @pytest.mark.django_db
-def test_current_model_factory(rel_current_factory):
+def test_rel_current_factory(rel_current_factory):
     rm = rel_current_factory.create()
-
     assert rm.current

2e14596af74c69601c65efc871c183c0b16a251f
SergeyK1983 <ssp-serg@yandex.ru>
2024-05-19 12:01:28 +0300

    add test_app_list_id

--- testpoject/testapp/tests/conftest.py
+++ testpoject/testapp/tests/conftest.py
@@ -11,3 +11,13 @@ register(RelCurrentFactory)  # => rel_current_factory
 @pytest.fixture
 def api_client():
     return APIClient
+
+
+@pytest.fixture(scope="session")
+@pytest.mark.django_db
+def get_some_current_obj(current_model_factory, rel_current_factory) -> None:
+    instance = current_model_factory.create()
+    rel_current_factory.create(current=instance)
+    rel_current_factory.create(current=instance)
+
+    return None
--- testpoject/testapp/tests/test_urls.py
+++ testpoject/testapp/tests/test_urls.py
@@ -7,6 +7,7 @@ from rest_framework import status
 @pytest.mark.django_db
 def test_app_list(api_client, current_model_factory):
     endpoint = f"{reverse('app-list')}"
+
     inst = current_model_factory.create()
     current_model_factory.create()
 
@@ -17,3 +18,18 @@ def test_app_list(api_client, current_model_factory):
     assert len(content) == 2
     assert len(content[0]) == 5
     assert response.data[0]["name"] == inst.name  # то же самое что и content, но OrderedDict
+
+
+@pytest.mark.django_db
+def test_app_list_id(api_client, current_model_factory, rel_current_factory):
+    instance = current_model_factory.create()
+    rel_current_factory.create(current=instance)
+    rel_current_factory.create(current=instance)
+
+    endpoint = f"{reverse('app-list-id', kwargs={'id': 1})}"
+    response = api_client().get(endpoint)
+    data = response.data
+
+    assert response.status_code == status.HTTP_200_OK
+    assert len(data) == 1
+    assert len(data[0]["current"]) == 2

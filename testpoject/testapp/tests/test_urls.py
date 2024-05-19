741b11aa1df0464b57d949a2f8bdcc3814ea9972
SergeyK1983 <ssp-serg@yandex.ru>
2024-05-19 14:16:07 +0300

    add test_current_create

--- testpoject/testapp/tests/test_urls.py
+++ testpoject/testapp/tests/test_urls.py
@@ -2,6 +2,9 @@ import json
 import pytest
 from django.urls import reverse
 from rest_framework import status
+from django.test.client import encode_multipart
+
+from ..models import CurrentModel
 
 
 @pytest.mark.django_db
@@ -33,3 +36,19 @@ def test_app_list_id(api_client, current_model_factory, rel_current_factory):
     assert response.status_code == status.HTTP_200_OK
     assert len(data) == 1
     assert len(data[0]["current"]) == 2
+
+
+@pytest.mark.django_db
+def test_current_create(api_client, current_model_factory, rel_current_factory):
+    endpoint = f"{reverse('current-create')}"
+
+    data = {'name': 'serg', 'description': 'sfsdfsdf', 'count': 2}
+    content = encode_multipart('BoUnDaRyStRiNg', data)
+    content_type = 'multipart/form-data; boundary=BoUnDaRyStRiNg'
+
+    response = api_client().post(endpoint, data=content, content_type=content_type)
+    cur = CurrentModel.objects.all().last()
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.data == data
+    assert cur.name == data["name"]

12c1500b2415ef73feff2f6fef8e85ccb6367cca
SergeyK1983 <ssp-serg@yandex.ru>
2024-05-30 16:32:42 +0300

    commit three

--- testpoject/testapp/tests/conftest.py
+++ testpoject/testapp/tests/conftest.py
@@ -7,7 +7,7 @@ from .factories import CurrentModelFactory, RelCurrentFactory
 register(CurrentModelFactory)  # => current_model_factory
 register(RelCurrentFactory)  # => rel_current_factory
 
-# примечание стереть
+
 @pytest.fixture
 def api_client():
     return APIClient
